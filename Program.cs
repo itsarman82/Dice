98810c90d8721a25edc7bdf1844232bce53589dc
arman sareskandi <42113160+itsarman82@users.noreply.github.com>
2022-09-13 16:05:49 +0430

    First commitment of my dice application

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocme to Dice this program make 2 random dice resaults.");
+
+            Random random = new Random();
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+
+            Console.WriteLine(dice1);
+            Console.WriteLine(dice2);
+        }
+    }
+}
